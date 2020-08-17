8cba008ef413b0fe0a306177b91e5a22c1db9023
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-17 15:31:30 -0700

    Added functionality for objects and arrays. Completed all test cases. Assignment Complete

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,29 +4,38 @@
 // but you don't so you're going to write it from scratch:
 
 //Splitting the object into parts
-  // { } , [ ],
-  // Add " " each individual compentnt
-  // Check bools
-  //
+// { } , [ ],
+// Add " " each individual compentnt
+// Check bools
+//
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   let result = [];
 
-  if(typeof obj === 'string') {
+  if (typeof obj === 'string') {
     return '"' + obj + '"';
-  } else if(typeof obj === 'number' || type obj === 'boolean' || typeof obj === null) {
+  } else if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
     return String(obj);
-  } else if(obj === undefined || obj instanceof Function) {
-    return "";
+  } else if (obj === undefined || obj instanceof Function) {
+    return '';
   }
 
+  if (Array.isArray(obj)) {
+    obj.forEach(function (element) {
+      result.push(stringifyJSON(element));
+    });
+    return '[' + result + ']';
+  }
 
-  //remove the brackets from the current element
-  //stringify each element in the arr
-  // add those into the result
-
-    // [ a, [ b], [ c] ]
-    // ' [a, '[ b]', '[ c]' ]'
+  if (obj instanceof Object) {
+    for (var key in obj) {
+      if (obj[key] === undefined || obj[key] instanceof Function) {
+        continue;
+      }
+      result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    }
+    return '{' + result + '}';
+  }
 };
 
 
@@ -34,13 +43,13 @@ var stringifyJSON = function(obj) {
 // Result = [];
 // Check type of object(string/bool/arr/obj/num)
 // if array
-  //iterate through each element and use recursion function
-  //adress a single element and recur using funciton
-  //push to result arr
-  // result = result arr (returning)
+//iterate through each element and use recursion function
+//adress a single element and recur using funciton
+//push to result arr
+// result = result arr (returning)
 
 // if its an obj
-  //iterate through keys
-    //push to result as formated obj
-    // result = result arr(return)
+//iterate through keys
+//push to result as formated obj
+// result = result arr(return)
 
