242cd700962034efe99b12c6ddced0c08caf92f9
JacobDeCastroJohnson <Jacob.DeCastro.J@gmail.com>
2020-08-17 12:38:30 -0700

    Added cases for all types except objects and arrays to the function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,44 @@
 
 // but you don't so you're going to write it from scratch:
 
+//Splitting the object into parts
+  // { } , [ ],
+  // Add " " each individual compentnt
+  // Check bools
+  //
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let result = [];
+
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if(typeof obj === 'number' || type obj === 'boolean' || typeof obj === null) {
+    return String(obj);
+  } else if(obj === undefined || obj instanceof Function) {
+    return "";
+  }
+
+
+  //remove the brackets from the current element
+  //stringify each element in the arr
+  // add those into the result
+
+    // [ a, [ b], [ c] ]
+    // ' [a, '[ b]', '[ c]' ]'
 };
+
+
+
+// Result = [];
+// Check type of object(string/bool/arr/obj/num)
+// if array
+  //iterate through each element and use recursion function
+  //adress a single element and recur using funciton
+  //push to result arr
+  // result = result arr (returning)
+
+// if its an obj
+  //iterate through keys
+    //push to result as formated obj
+    // result = result arr(return)
+
